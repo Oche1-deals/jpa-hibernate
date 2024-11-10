e7fb5971b377e1374d6d44d32dabe31a799171e3
Oche1-deals <odohraphaeloche11@gmail.com>
2024-11-10 23:17:31 +0100

    using jdbc

--- jpa-and-hibernate/src/main/java/com/oche1/springboot/jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ jpa-and-hibernate/src/main/java/com/oche1/springboot/jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.oche1.springboot.jpa_and_hibernate.course.jdbc;
 
+import com.oche1.springboot.jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,13 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     private CourseJdbcRepository repository;
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1,"Learn AWS now","in28min"));
+        repository.insert(new Course(2,"Learn Azure now","in28min"));
+        repository.insert(new Course(3,"Learn Devops now","in28min"));
+
+        repository.deleteById(1);
+
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
     }
 }
